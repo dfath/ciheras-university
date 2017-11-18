72b5185f0a85c1f1f25bf4f39800486f538031a5
dfath <fathrico@gmail.com>
2017-11-18 17:12:58 +0700

    install pagekit

---
+++ cms/config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'ciheras_university',
+        'prefix' => 'cu_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'CMOT7nsX.f6wx1anEJ7SV/7jnFxzYKvlxiVqrjjdgcPMDEpfKlCkGQM6anNHla8t',
+  ),
+);
